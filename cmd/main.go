cbca7d420429af6119a51bcf0908268dd83d3048
William <william.knipe@googlemail.com>
2022-12-10 14:08:12 +0000

    Remove goroutine for making surrealDB connection

--- cmd/main.go
+++ cmd/main.go
@@ -4,7 +4,6 @@ import (
 	"fmt"
 	"log"
 	"net/http"
-	"sync"
 
 	"github.com/gorilla/mux"
 	"github.com/surrealdb/surrealdb.go"
@@ -14,18 +13,15 @@ import (
 func main() {
 	fmt.Println("Connecting to SurrealDB database")
 
-	c := make(chan *surrealdb.DB)
-
 	// Connect to Database via websocket
-	go InitDatabase(c)
-	db := <-c
-	fmt.Println(db)
+	db := InitDatabase()
+	defer db.Close()
 
 	fmt.Println("Starting server")
 	// Create a new router
 	router := mux.NewRouter()
 
-	// Handlers import from handler packages
+	// Handlers import from handlers package
 	HealthCheck := handlers.HealthCheck
 	AddCafes := handlers.AddCafes
 	GetCafes := handlers.GetCafes
@@ -47,71 +43,52 @@ type Cafe struct {
 	Chairs string `json:"chairs"`
 }
 
-func InitDatabase(c chan *surrealdb.DB) {
-	var wg sync.WaitGroup
-	wg.Add(1)
-
+func InitDatabase() *surrealdb.DB {
 	// Create new Surreal db connection.
-	db, err1 := surrealdb.New("ws://localhost:8000/rpc")
-
-	if err1 != nil {
+	db, err := surrealdb.New("ws://localhost:8000/rpc")
+	if err != nil {
 		fmt.Println("InitDatabase new db failed")
-		panic(err1)
+		panic(err)
 	}
 
-	defer db.Close()
-	fmt.Println(db)
-
 	// Credentials set in surrealDB start command
-	_, err2 := db.Signin(map[string]interface{}{
+	_, err = db.Signin(map[string]interface{}{
 		"user": "root",
 		"pass": "root",
 	})
-
-	if err2 != nil {
+	if err != nil {
 		fmt.Println("InitDatabase sign in falled")
-		panic(err2)
+		panic(err)
 	}
 
-	_, err := db.Use("test", "test")
-
+	_, err = db.Use("test", "test")
 	if err != nil {
 		fmt.Println("InitDatabase db.Use failed")
 		panic(err)
 	}
 
-	// Create cafes table
-	// Some example cafe data
+	// Create cafes table with some example cafe data
 	_, err = db.Create("cafes", Cafe{
 		Name:   "hot comfy cafe",
 		Coffee: "hot",
 		Chairs: "comfy",
 	})
-
 	if err != nil {
 		fmt.Println("InitDatabase db.Create failed")
 		panic(err)
 	}
-
-	userData, err4 := db.Select("cafes")
-
-	if err4 != nil {
-		fmt.Println("InitDatabase select cafes failed")
-		panic(err4)
-	}
-
 	log.Println("InitDatabase Created cafes")
+
+	userData, err := db.Select("cafes")
+	if err != nil {
+		fmt.Println("InitDatabase select cafes failed")
+		panic(err)
+	}
 
 	if userData == nil {
 		fmt.Println("InitDatabase Select cafes is nil")
 	}
 
 	fmt.Println("InitDatabase created DB")
-
-	c <- db
-
-	fmt.Println("Sent pointer through channel")
-
-	// Wait forever with this goroutine
-	wg.Wait()
+	return db
 }
